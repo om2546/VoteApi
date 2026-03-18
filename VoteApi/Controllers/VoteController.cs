ca83770453a75643a0fc85d834a88d727c9120fe
Chayarob (Aom) Chantrapiwat <chayarob.cha@quantium.pe>
2026-03-18 15:15:15 +0700

    Add create vote endpoint

---
+++ VoteApi/Controllers/VoteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VoteApi.Models;
+
+namespace VoteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoteController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VoteController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VoteItem>> CreateVote([FromBody] VoteItem voteItem)
+        {
+            if (voteItem == null)
+            {
+                return BadRequest("Vote item cannot be null");
+            }
+
+            voteItem.CreatedAt = DateTime.UtcNow;
+
+            foreach (var option in voteItem.VoteOptions)
+            {
+                option.VoteCount = 0;
+            }
+
+            _context.VoteItems.Add(voteItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(CreateVote), new { id = voteItem.Id }, voteItem);
+        }
+
+    }
+}
