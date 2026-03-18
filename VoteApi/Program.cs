89700c048fb3aa840d9006f405f22557263ef457
Chayarob (Aom) Chantrapiwat <chayarob.cha@quantium.pe>
2026-03-18 15:12:23 +0700

    Initial create

---
+++ VoteApi/Program.cs
@@ -0,0 +1,23 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
