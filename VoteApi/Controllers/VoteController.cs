3f4bbf09e5eab60b80c409debb09d96ca0d103b3
Chayarob (Aom) Chantrapiwat <chayarob.cha@quantium.pe>
2026-03-18 15:16:18 +0700

    Add endpoint for get vote with pagination

--- VoteApi/Controllers/VoteController.cs
+++ VoteApi/Controllers/VoteController.cs
@@ -36,5 +36,32 @@ namespace VoteApi.Controllers
             return CreatedAtAction(nameof(CreateVote), new { id = voteItem.Id }, voteItem);
         }
 
+        [HttpGet]
+        public async Task<ActionResult> GetVotes([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            if (page < 1)
+                page = 1;
+            
+            if (pageSize < 1)
+                pageSize = 10;
+
+            var totalItems = await _context.VoteItems.CountAsync();
+
+            var voteItems = await _context.VoteItems
+                .Include(v => v.VoteOptions)
+                .OrderBy(v => v.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            
+            return Ok(new 
+            {
+                items = voteItems,
+                page,
+                pageSize,
+                totalItems,
+                totalPages = (int)Math.Ceiling(totalItems / (double)pageSize)
+            });
+        }
     }
 }
